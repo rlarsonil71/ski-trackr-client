0995553b938ab5b0fda68de2c3ababd815a8a5fc
rlarsonil71 <ron.larson@libertymutual.com>
2017-06-06 21:53:28 -0400

    Added click event handlers for trip resource
    Added clear text of click event handlers for trip resource (log-my-trip
     modal)

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,6 +3,7 @@
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 const authEvents = require('./auth/events.js')
+const tripEvents = require('./trips/events.js')
 
 $(() => {
   setAPIOrigin(location, config)
@@ -10,6 +11,7 @@ $(() => {
   // View options are 'header-view' 'drawer-view' 'sign-in-view' 'sign-up-view' 'change-password-view'
   // console.log('Page loaded')
   authEvents.addHandlers()
+  tripEvents.addTripHandlers()
 
   // Clear text from click event handlers for USER authentication
   $('#sign-up-modal').on('click', function () { $('#tempSignUpError').html('') })
@@ -21,6 +23,10 @@ $(() => {
   $('#chng-pw-modal').on('click', function () { $('#tempChangePasswordError').html('') })
   $('#chng-pw-modal').on('click', function () { $('#change-password').find('input:text, input:password, select, textarea').val('') })
 
+  // Clear text from click event handlers for TRIP resource
+  $('#log-my-trip-modal').on('click', function () { $('#tempLogMyTripError').html('') })
+  $('#log-my-trip-modal').on('click', function () { $('#log-my-trip').find('input:text, select, textarea').val('') })
+
   // Upon page load, hide Change Password and Sign Out dropdown menu options
   $('#chng-pw-modal').hide()
   $('#sign-out-modal').hide()
@@ -28,6 +34,9 @@ $(() => {
   // Upon page load, show Sign Up and Sign In dropdown menu options
   $('#sign-up-modal').show()
   $('#sign-in-modal').show()
+
+  // Upon page load, hide Trip dropdown modal
+  $('#trip-dropdown-modal').hide()
 
   // Upon page load, hide Alert Id popup text
   $('#alert-id').hide()
