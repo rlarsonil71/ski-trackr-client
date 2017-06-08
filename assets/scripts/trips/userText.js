21f241e35d7821856bf2aa77f22bc49b8779cb9a
rlarsonil71 <ron.larson@libertymutual.com>
2017-06-08 13:43:26 -0400

    Added user error text when user updates a selected trip
    Added the following user text functions:
     - updateMyTripSuccess
     - updateMyTripError (when all form fields are not entered)
     - updateMyTripFutureDateError (when selected date is in the future -
         which is invalid for a tripDate)

--- assets/scripts/trips/userText.js
+++ assets/scripts/trips/userText.js
@@ -12,8 +12,23 @@ const logMyTripFutureDateError = function () {
   $('#tempLogMyTripError').html("Invalid date selected.  Please select today's date or a previous date.")
 }
 
+const updateMyTripSuccess = function () {
+  $('#tempUpdateMyTripError').html('')
+}
+
+const updateMyTripError = function () {
+  $('#tempUpdateMyTripError').html('All fields are required.  Please verify all fields are filled in.')
+}
+
+const updateMyTripFutureDateError = function () {
+  $('#tempUpdateMyTripError').html("Invalid date selected.  Please select today's date or a previous date.")
+}
+
 module.exports = {
   logMyTripSuccess,
   logMyTripError,
-  logMyTripFutureDateError
+  logMyTripFutureDateError,
+  updateMyTripSuccess,
+  updateMyTripError,
+  updateMyTripFutureDateError
 }
